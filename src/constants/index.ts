d919ee5a8d7893bec0d6ae403efeedaab69e4e31
Eldrige <peldrige8@gmail.com>
2024-11-27 18:38:42 +0200

    add new footer link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,78 +1,79 @@
-import { FiSun } from "react-icons/fi";
-import { BsMoonStarsFill } from "react-icons/bs";
-import { GoDeviceDesktop } from "react-icons/go";
-import { AiOutlineHome } from "react-icons/ai";
-import { TbMovie } from "react-icons/tb";
-import { MdOutlineLiveTv } from "react-icons/md";
+import { FiSun } from 'react-icons/fi';
+import { BsMoonStarsFill } from 'react-icons/bs';
+import { GoDeviceDesktop } from 'react-icons/go';
+import { AiOutlineHome } from 'react-icons/ai';
+import { TbMovie } from 'react-icons/tb';
+import { MdOutlineLiveTv } from 'react-icons/md';
 
-import { ITheme, INavLink } from "../types";
+import { ITheme, INavLink } from '../types';
 
 export const navLinks: INavLink[] = [
   {
-    title: "home",
-    path: "/",
+    title: 'home',
+    path: '/',
     icon: AiOutlineHome,
   },
   {
-    title: "movies",
-    path: "/movie",
+    title: 'movies',
+    path: '/movie',
     icon: TbMovie,
   },
   {
-    title: "tv series",
-    path: "/tv",
+    title: 'tv series',
+    path: '/tv',
     icon: MdOutlineLiveTv,
   },
 ];
 
 export const themeOptions: ITheme[] = [
   {
-    title: "Dark",
+    title: 'Dark',
     icon: BsMoonStarsFill,
   },
   {
-    title: "Light",
+    title: 'Light',
     icon: FiSun,
   },
   {
-    title: "System",
+    title: 'System',
     icon: GoDeviceDesktop,
   },
 ];
 
 export const footerLinks = [
-  "home",
-  "live",
-  "you must watch",
-  "contact us",
-  "FAQ",
-  "Recent release",
-  "term of services",
-  "premium",
-  "Top IMDB",
-  "About us",
-  "Privacy policy",
+  'home',
+  'live',
+  'you must watch',
+  'contact us',
+  'FAQ',
+  'Recent release',
+  'term of services',
+  'premium',
+  'Top IMDB',
+  'About us',
+  'Privacy policy',
+  'top',
 ];
 
 export const sections = [
   {
-    title: "Trending movies",
-    category: "movie",
-    type: "popular",
+    title: 'Trending movies',
+    category: 'movie',
+    type: 'popular',
   },
   {
-    title: "Top rated movies",
-    category: "movie",
-    type: "top_rated",
+    title: 'Top rated movies',
+    category: 'movie',
+    type: 'top_rated',
   },
   {
-    title: "Trending series",
-    category: "tv",
-    type: "popular",
+    title: 'Trending series',
+    category: 'tv',
+    type: 'popular',
   },
   {
-    title: "Top rated series",
-    category: "tv",
-    type: "top_rated",
+    title: 'Top rated series',
+    category: 'tv',
+    type: 'top_rated',
   },
 ];
